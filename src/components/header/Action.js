b4da7ceddda316cb983a03d055151b7c29bd9232
GOKUL P <141717721+Gokudsp@users.noreply.github.com>
2024-09-17 13:29:41 +0530

    Update Action.js

--- src/components/header/Action.js
+++ src/components/header/Action.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Action = () => {
   return (
     <div className='action'>
-        <a className='btn' href="https://drive.google.com/file/d/1P8oOutb3b32c1eNVqDCoWYSXn3ZirnA4/view?usp=drivesdk" target='_blank' download>Download Resume</a>
+        <a className='btn' href="https://drive.google.com/file/d/1VMCNWqbUdUa7hMSe8yU4p1xG4-WmCAWN/view?usp=drivesdk" target='_blank' download>Download Resume</a>
         <a className='btn btn-primary' href="#contact">Contact me!</a>
     </div>
   )
